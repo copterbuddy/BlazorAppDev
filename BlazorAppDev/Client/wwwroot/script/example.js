4d7df868667af8b64a84321c5b6e32d6a89d41bc
copterbuddy <copterbuddy@gmail.com>
2023-04-01 21:23:59 +0700

    first commit

---
+++ BlazorAppDev/Client/wwwroot/script/example.js
@@ -0,0 +1,9 @@
+function showAlert(message) {
+    alert(message + "from index.html");
+}
+function showCal(a, b) {
+    return a + b;
+}
+function consoleLog(message) {
+    console.log(message);
+}
---
+++ BlazorAppDev/Client/wwwroot/script/example.ts
@@ -0,0 +1,11 @@
+function showAlert(message: string): void {
+    alert(message + "from index.html");
+}
+
+function showCal(a: number, b: number): number {
+    return a + b
+}
+
+function consoleLog(message: string) {
+    console.log(message);
+}
