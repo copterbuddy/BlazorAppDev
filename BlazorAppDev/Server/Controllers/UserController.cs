10bfb70f86a1c21114f4a4e94c9e7aade5d068f8
copterbuddy <copterbuddy@gmail.com>
2023-04-02 20:05:50 +0700

    add user controller

---
+++ BlazorAppDev/Server/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using BlazorAppDev.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BlazorAppDev.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public UserController(ILogger<UserController> logger,IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet("Greeting")]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Greeting Log");
+
+            return Ok($"Service Running On {_configuration.GetValue<string>("ASPNETCORE_ENVIRONMENT")}");
+        }
+    }
+}
