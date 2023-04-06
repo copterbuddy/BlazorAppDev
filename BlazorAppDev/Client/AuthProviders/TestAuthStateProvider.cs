6b7c816a5b7706489f47b33f9c26e5466b3adcc1
copterbuddy <copterbuddy@gmail.com>
2023-04-06 14:10:50 +0700

    add authstateprovider

---
+++ BlazorAppDev/Client/AuthProviders/TestAuthStateProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorAppDev.Client.AuthProviders
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(1500);
+            var claim = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Cop Ter"),
+                new Claim(ClaimTypes.Role,"Admin"),
+            };
+
+            var anonymous = new ClaimsIdentity(claim, "testAuthType");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymous)));
+        }
+    }
+}
--- BlazorAppDev/Client/Program.cs
+++ BlazorAppDev/Client/Program.cs
@@ -1,6 +1,8 @@
 using BlazorAppDev.Client;
+using BlazorAppDev.Client.AuthProviders;
 using BlazorAppDev.Shared.ValidatorModels;
 using FluentValidation;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -13,6 +15,9 @@ namespace BlazorAppDev.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
