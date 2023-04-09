916a0fef3af3f519867813b4af3239005913dc31
copterbuddy <copterbuddy@gmail.com>
2023-04-09 10:06:20 +0700

    add unit test register

--- BlazorAppDev/Test/Client/Controllers/UserControllerTest.cs
+++ BlazorAppDev/Test/Client/Controllers/UserControllerTest.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Moq;
+using BlazorAppDev.Shared.Models;
 
 namespace BlazorAppDev.Test.Client.Controllers
 {
@@ -40,6 +41,7 @@ namespace BlazorAppDev.Test.Client.Controllers
             // Dispose here
         }
 
+        #region User/Greeting
         [Fact]
         public void Get_ReturnsOkObjectResult()
         {
@@ -64,5 +66,57 @@ namespace BlazorAppDev.Test.Client.Controllers
             // Assert
             Assert.Equal(expectedValue, result.Value);
         }
+        #endregion User/Greeting
+
+        #region User/Register
+        [Fact]
+        public async Task Register_ValidRequest_ReturnsOkResult()
+        {
+            // Arrange
+            var request = new RegisterRequest
+            {
+                Email = "test@example.com",
+                Password = "password",
+                ConfirmPassword = "password"
+            };
+
+            var userServiceMock = new Mock<IUserService>();
+            userServiceMock.Setup(x => x.Register(request)).ReturnsAsync(true);
+
+            var loggerMock = new Mock<ILogger<UserController>>();
+
+            var configurationMock = new Mock<IConfiguration>();
+
+            controller = new UserController(loggerMock.Object, configurationMock.Object, userServiceMock.Object);
+
+            // Act
+            var result = await controller.Register(request);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var response = Assert.IsType<RegisterResponse>(okResult.Value);
+            Assert.True(response.Result);
+        }
+
+        [Fact]
+        public async Task Register_InvalidRequest_ReturnsUnauthorizedResult()
+        {
+            // Arrange
+            var request = new RegisterRequest
+            {
+                Email = "test@example.com",
+                Password = "password",
+                ConfirmPassword = "wrongpassword"
+            };
+
+            controller = new UserController(null, null, null);
+
+            // Act
+            var result = await controller.Register(request);
+
+            // Assert
+            Assert.IsType<UnauthorizedResult>(result);
+        }
+        #endregion User/Register
     }
 }
