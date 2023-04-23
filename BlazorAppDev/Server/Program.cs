5bafd48fe5acc8f62f174eb7e57d2a679e0125fd
copterbuddy <copterbuddy@gmail.com>
2023-04-23 11:35:31 +0700

    comment migration for test

--- BlazorAppDev/Server/Program.cs
+++ BlazorAppDev/Server/Program.cs
@@ -53,11 +53,11 @@ namespace BlazorAppDev
 
             var app = builder.Build();
 
-            using (var scope = app.Services.CreateScope())
-            {
-                var db = scope.ServiceProvider.GetRequiredService<MyDbContext>();
-                await db.Database.MigrateAsync();
-            }
+            //using (var scope = app.Services.CreateScope())
+            //{
+            //    var db = scope.ServiceProvider.GetRequiredService<MyDbContext>();
+            //    await db.Database.MigrateAsync();
+            //}
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
